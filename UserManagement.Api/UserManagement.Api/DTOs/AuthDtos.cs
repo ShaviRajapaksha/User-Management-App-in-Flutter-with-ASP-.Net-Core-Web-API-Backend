3e7a1a2357ac695963b4a99f571654a067db8922
IT20272654 <shavindurajapaksha@gmail.com>
2026-01-03 01:21:42 +0530

    Add Auth DTOs for user registration and login
    Introduced RegisterDto and LoginDto classes to handle user registration and login data transfer. These DTOs will be used to encapsulate authentication-related request data.

---
+++ UserManagement.Api/UserManagement.Api/DTOs/AuthDtos.cs
@@ -0,0 +1,15 @@
+﻿namespace UserManagement.Api.DTOs
+{
+    public class RegisterDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public String Role { get; set; } = "User";
+    }
+
+    public class LoginDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
