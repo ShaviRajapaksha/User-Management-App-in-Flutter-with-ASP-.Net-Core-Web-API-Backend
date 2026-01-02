c4dbf8493900453fa4ef9e8ec3b5a8fe8700b9aa
IT20272654 <shavindurajapaksha@gmail.com>
2026-01-02 21:17:40 +0530

    Add User model class
    Introduces the User model with Id, Username, PasswordHash, and Role properties for user management functionality.

---
+++ UserManagement.Api/UserManagement.Api/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserManagement.Api.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public String Username { get; set; }
+        public String PasswordHash { get; set; }
+        public String Role { get; set; } = "User";
+    }
+}
