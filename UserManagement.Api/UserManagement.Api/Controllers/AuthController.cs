d810c8ea3be5dcf8987c7ba1390e3e6533b1e3e0
IT20272654 <shavindurajapaksha@gmail.com>
2026-01-03 01:21:04 +0530

    Create AuthController.cs

---
+++ UserManagement.Api/UserManagement.Api/Controllers/AuthController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Scripting;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using UserManagement.Api.Data;
+using UserManagement.Api.DTOs;
+using UserManagement.Api.Models;
+
+
+namespace UserManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private readonly IConfiguration _config;
+        public AuthController(AppDbContext context, IConfiguration config)
+        {
+            _context = context;
+            _config = config;
+        }
+
+        // POST: api/Auth
+        [HttpPost("register")]
+        public IActionResult Register(RegisterDto dto)
+        {
+            if (_context.Users.Any(u => u.Username == dto.Username))
+                return BadRequest("User already exists");
+
+            var user = new User
+            {
+                Username = dto.Username,
+                PasswordHash = BCrypt.Net.BCrypt.HashPassword(dto.Password),
+                Role = dto.Role
+            };
+
+            _context.Users.Add(user);
+            _context.SaveChanges();
+
+            return Ok("User registered");
+        }
+        [HttpPost("login")]
+        public IActionResult Login(LoginDto dto)
+        {
+            var user = _context.Users.SingleOrDefault(u => u.Username == dto.Username);
+            if (user == null || !BCrypt.Net.BCrypt.Verify(dto.Password, user.PasswordHash))
+                return Unauthorized();
+
+            var claims = new[]
+            {
+            new Claim(ClaimTypes.Name, user.Username),
+            new Claim(ClaimTypes.Role, user.Role)
+        };
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_config["Jwt:Key"]!));
+
+            var token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddHours(2),
+                signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha256)
+            );
+
+            return Ok(new
+            {
+                token = new JwtSecurityTokenHandler().WriteToken(token),
+                role = user.Role
+            });
+        }
+    }
+}
