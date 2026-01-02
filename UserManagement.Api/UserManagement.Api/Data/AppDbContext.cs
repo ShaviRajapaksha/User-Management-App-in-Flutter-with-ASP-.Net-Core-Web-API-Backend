6fc3ddc5f265cef0c58f7efe5a164be1b32bb82a
IT20272654 <shavindurajapaksha@gmail.com>
2026-01-02 21:17:33 +0530

    Add AppDbContext with Users DbSet
    Introduces the AppDbContext class inheriting from DbContext, including a DbSet for User entities. This sets up the Entity Framework Core context for user management.

---
+++ UserManagement.Api/UserManagement.Api/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserManagement.Api.Models;
+
+namespace UserManagement.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+        public DbSet<User> Users => Set<User>();
+    }
+}
