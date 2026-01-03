d0be6b1fd0fa7976039b1639749f9035a807bf30
IT20272654 <shavindurajapaksha@gmail.com>
2026-01-04 01:00:01 +0530

    feat: Implement LoginScreen with authentication functionality

---
+++ frontend_ui/lib/screens/login_screen.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+import '../api/api_service.dart';
+import '../utils/auth_storage.dart';
+import 'admin_home.dart';
+import 'user_home.dart';
+
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final userNameController = TextEditingController();
+  final passwordController = TextEditingController();
+  bool loading = false;
+
+  void login() async {
+    setState(() => loading = true);
+
+    try {
+      final response = await ApiService.login(
+        userNameController.text,
+        passwordController.text,
+      );
+      await AuthStorage.saveToken(response['token'], response['role']);
+      //use result if response is not working
+      if (response['role'] == 'Admin') {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (_) => const AdminHome()),
+        );
+      } else {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (_) => const UserHome()),
+        );
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text("Invalid Login")));
+    } finally {
+      setState(() => loading = false);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Login')),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(children: [
+          TextField(
+            controller: userNameController,
+            decoration: InputDecoration(labelText: 'Username'),
+          ),
+          TextField(
+            controller: passwordController,
+            decoration: InputDecoration(labelText: 'Password'),
+            obscureText: true,
+          ),
+          SizedBox(height: 20),
+          loading
+              ? CircularProgressIndicator()
+              : ElevatedButton(
+                  onPressed: login,
+                  child: Text('Login'),
+                ),
+      ],),
+      ),
+    );
+  }
+}
