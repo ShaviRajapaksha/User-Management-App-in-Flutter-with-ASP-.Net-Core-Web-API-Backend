34f9790963ff5615b1446e8ac97e70cc2f8becc8
IT20272654 <shavindurajapaksha@gmail.com>
2026-01-04 01:21:26 +0530

    feat: Add navigation to RegisterScreen from LoginScreen

--- frontend_ui/lib/screens/login_screen.dart
+++ frontend_ui/lib/screens/login_screen.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 import '../api/api_service.dart';
 import '../utils/auth_storage.dart';
 import 'admin_home.dart';
+import 'register_screen.dart';
 import 'user_home.dart';
-
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -54,24 +54,33 @@ class _LoginScreenState extends State<LoginScreen> {
       appBar: AppBar(title: Text('Login')),
       body: Padding(
         padding: EdgeInsets.all(16.0),
-        child: Column(children: [
-          TextField(
-            controller: userNameController,
-            decoration: InputDecoration(labelText: 'Username'),
-          ),
-          TextField(
-            controller: passwordController,
-            decoration: InputDecoration(labelText: 'Password'),
-            obscureText: true,
-          ),
-          SizedBox(height: 20),
-          loading
-              ? CircularProgressIndicator()
-              : ElevatedButton(
-                  onPressed: login,
-                  child: Text('Login'),
-                ),
-      ],),
+        child: Column(
+          children: [
+            TextField(
+              controller: userNameController,
+              decoration: InputDecoration(labelText: 'Username'),
+            ),
+            TextField(
+              controller: passwordController,
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            loading
+                ? CircularProgressIndicator()
+                : ElevatedButton(onPressed: login, child: Text('Login')),
+            SizedBox(height: 20),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => const RegisterScreen()),
+                );
+              },
+              child: const Text("Create an account"),
+            ),
+          ],
+        ),
       ),
     );
   }
