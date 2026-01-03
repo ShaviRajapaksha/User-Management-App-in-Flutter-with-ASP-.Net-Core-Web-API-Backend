c3ebed99038d25d6e2420d38aee6ea74d608b458
IT20272654 <shavindurajapaksha@gmail.com>
2026-01-04 01:00:08 +0530

    feat: Add AdminHome screen with logout functionality

---
+++ frontend_ui/lib/screens/admin_home.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import '../utils/auth_storage.dart';
+import 'login_screen.dart';
+
+class AdminHome extends StatelessWidget {
+  const AdminHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Admin Dashboard"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () async {
+              await AuthStorage.logout();
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (_) => const LoginScreen()),
+              );
+            },
+          ),
+        ],
+      ),
+      body: const Center(
+        child: Text("Welcome Admin 👑", style: TextStyle(fontSize: 22)),
+      ),
+    );
+  }
+}
